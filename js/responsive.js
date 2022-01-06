ee4048839fbe057b7347dfac43c8ccd890f79fc8
Истамов Бекзод <94726391+istamovbekzod@users.noreply.github.com>
2022-01-06 04:16:26 +0300

    Add files via upload

---
+++ js/responsive.js
@@ -0,0 +1,23 @@
+//Adaptive functions
+$(window).resize(function(event) {
+	adaptive_function();
+});
+function adaptive_header(w,h) {
+		var headerMenu=$('.header-menu');
+		var headerLang=$('.header-top-lang');
+	if(w<768){
+		if(!headerLang.hasClass('done')){
+			headerLang.addClass('done').appendTo(headerMenu);
+		}
+	}else{
+		if(headerLang.hasClass('done')){
+			headerLang.removeClass('done').prependTo($('.header-top'));
+		}
+	}
+}
+function adaptive_function() {
+		var w=$(window).outerWidth();
+		var h=$(window).outerHeight();
+	adaptive_header(w,h);
+}
+	adaptive_function();
